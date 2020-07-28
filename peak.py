d99e9a9558e9db1278bb624e79c786fbb4466ee2
Uchencho <aloziekelechi17@gmail.com>
2020-07-28 01:18:01 +0100

    login function done

--- peak.py
+++ peak.py
@@ -2,13 +2,12 @@ from selenium import webdriver
 import time
 from settings import PEAK_PASSWORD
 
-def peak_reg():
+def peak_reg(browser):
     '''
     Register as a student
     '''
     # Go to the webpage
     url = "https://sagittiform-forehea.000webhostapp.com/"
-    browser = webdriver.Chrome()
     browser.get(url)
     time.sleep(2)
 
@@ -30,3 +29,29 @@ def peak_reg():
     signUpBtn.click()
     time.sleep(10)
     print("Finshed registeration\n\n")
+
+
+def login(browser):
+    '''
+    Login student
+    '''
+    url = "https://sagittiform-forehea.000webhostapp.com/"
+    browser.get(url)
+    time.sleep(2)
+
+    # Fill in matric number and password
+    browser.find_element_by_id('matricno').send_keys("EEG/2011/999")
+    browser.find_element_by_id('password').send_keys(PEAK_PASSWORD)
+    time.sleep(0.5)
+
+    # Click sign in button
+    browser.find_element_by_class_name("typetwo").click()
+    time.sleep(5)
+
+    print("\n\n", browser.current_url, "\n\n")
+    return str(browser.current_url)
+
+
+if __name__ == "__main__":
+    browser = webdriver.Chrome()
+    login(browser)
